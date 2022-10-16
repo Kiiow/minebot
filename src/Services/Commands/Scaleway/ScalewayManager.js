e5f417e6d30474c9bbcbd452d7a194953372b6d7
Kiio__ <caillaud.hugo.44@gmail.com>
2022-10-16 23:11:10 +0200

    Add status and list command, start adding Start and Stop

--- src/Services/Commands/Scaleway/ScalewayManager.js
+++ src/Services/Commands/Scaleway/ScalewayManager.js
@@ -35,11 +35,10 @@ class ScalewayManager {
             .setPath("/zones/fr-par-1/servers");
         
         const response = await fetch(urlBuilder.build(), {
-            "method": "get",
-            "headers": { "X-Auth-Token": process.env.SCA_API_TOKEN }
+            method: "GET",
+            headers: { "X-Auth-Token": process.env.SCA_API_TOKEN }
         });
         const data = await response.json();
-        // console.log(data);
 
         if(!data.servers) {
             return this.interaction.reply(`Mmh u said list, right ?`);
@@ -56,20 +55,113 @@ class ScalewayManager {
                 { name: server.name, value: server.id, inline: true},
                 { name: "type", value: server.commercial_type, inline: true}
             );
-        })
+        });
         this.interaction.reply({ embeds: [embedListResponse] });
     }
 
-    status() {
-        this.interaction.reply(`Mmh u said status, right ? with id ${server_id}`);
+    async status() {
+        let urlBuilder = (new UrlBuilder("https://api.scaleway.com/instance/v1/"))
+            .setPath(`/zones/fr-par-1/servers/${this.serverId}`);
+        
+        const response = await fetch(urlBuilder.build(), {
+            method: "GET",
+            headers: { "X-Auth-Token": process.env.SCA_API_TOKEN }
+        });
+        const data = await response.json();
+        // console.log(data);
+
+        const embedListResponse = new EmbedBuilder()
+            .setColor(0x0099FF)
+            .setAuthor({ name: 'Status', iconURL: 'https://avatars.githubusercontent.com/u/5185491?s=200&v=4' })
+            .addFields(
+                { name: "Status", value: `${this.getColorByState(data.server.state)} ${data.server.state}`, inline: true},
+                { name: data.server.name, value: data.server.id, inline: true},
+                { name: "type", value: data.server.commercial_type, inline: true}
+            );
+        
+        this.interaction.reply({ embeds: [embedListResponse] });
     }
 
-    start() {
-        this.interaction.reply(`Mmh u said start, right ? with id ${server_id}`);
+    async start() {
+        let urlBuilderStart = (new UrlBuilder("https://api.scaleway.com/instance/v1/"))
+            .setPath(`/zones/fr-par-1/servers/${this.serverId}/action`);
+        
+        const startRequest = await fetch(urlBuilderStart.build(), {
+            method: "POST",
+            body: JSON.stringify({
+                action : "poweron"
+            }),
+            headers: {
+                "X-Auth-Token": process.env.SCA_API_TOKEN,
+                'Content-Type': 'application/json'
+            }
+        });
+        const result = await startRequest.json();
+        console.log(result);
+
+        this.interaction.reply("Starting server, maybe ...");
+
+        // let urlBuilder = (new UrlBuilder("https://api.scaleway.com/instance/v1/"))
+        //     .setPath(`/zones/fr-par-1/servers/${this.serverId}`);
+
+        // const statusRequest = await fetch(urlBuilder.build(), {
+        //     method: "GET",
+        //     headers: { "X-Auth-Token": process.env.SCA_API_TOKEN }
+        // });
+        // const data = await statusRequest.json();
+        // console.log(data);
+
+        // const embedListResponse = new EmbedBuilder()
+        //     .setColor(0x0099FF)
+        //     .setAuthor({ name: 'Start server', iconURL: 'https://avatars.githubusercontent.com/u/5185491?s=200&v=4' })
+        //     .addFields(
+        //         { name: "Status", value: `${this.getColorByState(data.server.state)} ${data.server.state}`, inline: true},
+        //         { name: data.server.name, value: data.server.id, inline: true},
+        //         { name: "type", value: data.server.commercial_type, inline: true}
+        //     );
+        
+        // this.interaction.reply({ embeds: [embedListResponse] });
     }
 
-    stop() {
-        this.interaction.reply(`You sure ? u said stop right ? with id ${server_id}`);
+    async stop() {
+        let urlBuilderStop = (new UrlBuilder("https://api.scaleway.com/instance/v1/"))
+            .setPath(`/zones/fr-par-1/servers/${this.serverId}/action`);
+        
+        const stopRequest = await fetch(urlBuilderStop.build(), {
+            method: "POST",
+            body: JSON.stringify({
+                "action": "poweroff"
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+                "X-Auth-Token": process.env.SCA_API_TOKEN
+            }
+        });
+        const result = await stopRequest.json();
+        console.log(result);
+
+        this.interaction.reply("Stopping server, maybe ...");
+
+        // let urlBuilder = (new UrlBuilder("https://api.scaleway.com/instance/v1/"))
+        //     .setPath(`/zones/fr-par-1/servers/${this.serverId}`);
+
+        // const statusRequest = await fetch(urlBuilder.build(), {
+        //     "method": "get",
+        //     "headers": { "X-Auth-Token": process.env.SCA_API_TOKEN }
+        // });
+        // const data = await statusRequest.json();
+        // console.log(data);
+
+        // const embedListResponse = new EmbedBuilder()
+        //     .setColor(0x0099FF)
+        //     .setAuthor({ name: 'Stop server', iconURL: 'https://avatars.githubusercontent.com/u/5185491?s=200&v=4' })
+        //     .addFields(
+        //         { name: "Status", value: `${this.getColorByState(data.server.state)} ${data.server.state}`, inline: true},
+        //         { name: data.server.name, value: data.server.id, inline: true},
+        //         { name: "type", value: data.server.commercial_type, inline: true}
+        //     );
+        
+        // this.interaction.reply({ embeds: [embedListResponse] });
     }
 }
 
